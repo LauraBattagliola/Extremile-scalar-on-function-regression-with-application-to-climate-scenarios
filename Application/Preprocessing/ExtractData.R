cacc67f71bd3ae5786f8007aa0b8950bc14cc2a0
LauraBattagliola <114673193+LauraBattagliola@users.noreply.github.com>
2024-05-30 17:35:36 +0200

    Extract data

---
+++ Application/Preprocessing/ExtractData.R
@@ -0,0 +1,355 @@
+################################################################################
+########################### Save data in lists #################################
+################################################################################
+
+setwd("/Code/Application/Data/DownloadedData/klimaszenarien-raumklima-ABO")
+ABO.RCP26.FF.WS <- read.csv("ABO_2060_RCP26_1in10-warmsummer.csv")
+ABO.RCP26.FF.DRY <- read.csv("ABO_2060_RCP26_DRY.csv")
+ABO.RCP85.FF.WS <- read.csv("ABO_2060_RCP85_1in10-warmsummer.csv")
+ABO.RCP85.FF.DRY <- read.csv("ABO_2060_RCP85_DRY.csv")
+
+
+setwd("/Code/Application/Data/DownloadedData/klimaszenarien-raumklima-AIG")
+AIG.RCP26.FF.WS <- read.csv("AIG_2060_RCP26_1in10-warmsummer.csv")
+AIG.RCP26.FF.DRY <- read.csv("AIG_2060_RCP26_DRY.csv")
+AIG.RCP85.FF.WS <- read.csv("AIG_2060_RCP85_1in10-warmsummer.csv")
+AIG.RCP85.FF.DRY <- read.csv("AIG_2060_RCP85_DRY.csv")
+
+setwd("/Code/Application/Data/DownloadedData/klimaszenarien-raumklima-ALT")
+ALT.RCP26.FF.WS <- read.csv("ALT_2060_RCP26_1in10-warmsummer.csv")
+ALT.RCP26.FF.DRY <- read.csv("ALT_2060_RCP26_DRY.csv")
+ALT.RCP85.FF.WS <- read.csv("ALT_2060_RCP85_1in10-warmsummer.csv")
+ALT.RCP85.FF.DRY <- read.csv("ALT_2060_RCP85_DRY.csv")
+
+
+setwd("/Code/Application/Data/DownloadedData/klimaszenarien-raumklima-BAS")
+BAS.RCP26.FF.WS <- read.csv("BAS_2060_RCP26_1in10-warmsummer.csv")
+BAS.RCP26.FF.DRY <- read.csv("BAS_2060_RCP26_DRY.csv")
+BAS.RCP85.FF.WS <- read.csv("BAS_2060_RCP85_1in10-warmsummer.csv")
+BAS.RCP85.FF.DRY <- read.csv("BAS_2060_RCP85_DRY.csv")
+
+setwd("/Code/Application/Data/DownloadedData/klimaszenarien-raumklima-BER")
+BER.RCP26.FF.WS <- read.csv("BER_2060_RCP26_1in10-warmsummer.csv")
+BER.RCP26.FF.DRY <- read.csv("BER_2060_RCP26_DRY.csv")
+BER.RCP85.FF.WS <- read.csv("BER_2060_RCP85_1in10-warmsummer.csv")
+BER.RCP85.FF.DRY <- read.csv("BER_2060_RCP85_DRY.csv")
+
+
+setwd("/Code/Application/Data/DownloadedData/klimaszenarien-raumklima-BKLI")
+BKLI.RCP26.FF.WS <- read.csv("BKLI_2060_RCP26_1in10-warmsummer.csv")
+BKLI.RCP26.FF.DRY <- read.csv("BKLI_2060_RCP26_DRY.csv")
+BKLI.RCP85.FF.WS <- read.csv("BKLI_2060_RCP85_1in10-warmsummer.csv")
+BKLI.RCP85.FF.DRY <- read.csv("BKLI_2060_RCP85_DRY.csv")
+
+
+setwd("/Code/Application/Data/DownloadedData/klimaszenarien-raumklima-BUS")
+BUS.RCP26.FF.WS <- read.csv("BUS_2060_RCP26_1in10-warmsummer.csv")
+BUS.RCP26.FF.DRY <- read.csv("BUS_2060_RCP26_DRY.csv")
+BUS.RCP85.FF.WS <- read.csv("BUS_2060_RCP85_1in10-warmsummer.csv")
+BUS.RCP85.FF.DRY <- read.csv("BUS_2060_RCP85_DRY.csv")
+
+
+setwd("/Code/Application/Data/DownloadedData/klimaszenarien-raumklima-CDF")
+CDF.RCP26.FF.WS <- read.csv("CDF_2060_RCP26_1in10-warmsummer.csv")
+CDF.RCP26.FF.DRY <- read.csv("CDF_2060_RCP26_DRY.csv")
+CDF.RCP85.FF.WS <- read.csv("CDF_2060_RCP85_1in10-warmsummer.csv")
+CDF.RCP85.FF.DRY <- read.csv("CDF_2060_RCP85_DRY.csv")
+
+
+setwd("/Code/Application/Data/DownloadedData/klimaszenarien-raumklima-CHU")
+CHU.RCP26.FF.WS <- read.csv("CHU_2060_RCP26_1in10-warmsummer.csv")
+CHU.RCP26.FF.DRY <- read.csv("CHU_2060_RCP26_DRY.csv")
+CHU.RCP85.FF.WS <- read.csv("CHU_2060_RCP85_1in10-warmsummer.csv")
+CHU.RCP85.FF.DRY <- read.csv("CHU_2060_RCP85_DRY.csv")
+
+
+setwd("/Code/Application/Data/DownloadedData/klimaszenarien-raumklima-DAV")
+DAV.RCP26.FF.WS <- read.csv("DAV_2060_RCP26_1in10-warmsummer.csv")
+DAV.RCP26.FF.DRY <- read.csv("DAV_2060_RCP26_DRY.csv")
+DAV.RCP85.FF.WS <- read.csv("DAV_2060_RCP85_1in10-warmsummer.csv")
+DAV.RCP85.FF.DRY <- read.csv("DAV_2060_RCP85_DRY.csv")
+
+
+setwd("/Code/Application/Data/DownloadedData/klimaszenarien-raumklima-DIS")
+DIS.RCP26.FF.WS <- read.csv("DIS_2060_RCP26_1in10-warmsummer.csv")
+DIS.RCP26.FF.DRY <- read.csv("DIS_2060_RCP26_DRY.csv")
+DIS.RCP85.FF.WS <- read.csv("DIS_2060_RCP85_1in10-warmsummer.csv")
+DIS.RCP85.FF.DRY <- read.csv("DIS_2060_RCP85_DRY.csv")
+
+
+setwd("/Code/Application/Data/DownloadedData/klimaszenarien-raumklima-ENG")
+ENG.RCP26.FF.WS <- read.csv("ENG_2060_RCP26_1in10-warmsummer.csv")
+ENG.RCP26.FF.DRY <- read.csv("ENG_2060_RCP26_DRY.csv")
+ENG.RCP85.FF.WS <- read.csv("ENG_2060_RCP85_1in10-warmsummer.csv")
+ENG.RCP85.FF.DRY <- read.csv("ENG_2060_RCP85_DRY.csv")
+
+
+setwd("/Code/Application/Data/DownloadedData/klimaszenarien-raumklima-FRE")
+FRE.RCP26.FF.WS <- read.csv("FRE_2060_RCP26_1in10-warmsummer.csv")
+FRE.RCP26.FF.DRY <- read.csv("FRE_2060_RCP26_DRY.csv")
+FRE.RCP85.FF.WS <- read.csv("FRE_2060_RCP85_1in10-warmsummer.csv")
+FRE.RCP85.FF.DRY <- read.csv("FRE_2060_RCP85_DRY.csv")
+
+
+setwd("/Code/Application/Data/DownloadedData/klimaszenarien-raumklima-GLA")
+GLA.RCP26.FF.WS <- read.csv("GLA_2060_RCP26_1in10-warmsummer.csv")
+GLA.RCP26.FF.DRY <- read.csv("GLA_2060_RCP26_DRY.csv")
+GLA.RCP85.FF.WS <- read.csv("GLA_2060_RCP85_1in10-warmsummer.csv")
+GLA.RCP85.FF.DRY <- read.csv("GLA_2060_RCP85_DRY.csv")
+
+
+setwd("/Code/Application/Data/DownloadedData/klimaszenarien-raumklima-GSB")
+GSB.RCP26.FF.WS <- read.csv("GSB_2060_RCP26_1in10-warmsummer.csv")
+GSB.RCP26.FF.DRY <- read.csv("GSB_2060_RCP26_DRY.csv")
+GSB.RCP85.FF.WS <- read.csv("GSB_2060_RCP85_1in10-warmsummer.csv")
+GSB.RCP85.FF.DRY <- read.csv("GSB_2060_RCP85_DRY.csv")
+
+setwd("/Code/Application/Data/DownloadedData/klimaszenarien-raumklima-GUT")
+GUT.RCP26.FF.WS <- read.csv("GUT_2060_RCP26_1in10-warmsummer.csv")
+GUT.RCP26.FF.DRY <- read.csv("GUT_2060_RCP26_DRY.csv")
+GUT.RCP85.FF.WS <- read.csv("GUT_2060_RCP85_1in10-warmsummer.csv")
+GUT.RCP85.FF.DRY <- read.csv("GUT_2060_RCP85_DRY.csv")
+
+
+setwd("/Code/Application/Data/DownloadedData/klimaszenarien-raumklima-GVE")
+GVE.RCP26.FF.WS <- read.csv("GVE_2060_RCP26_1in10-warmsummer.csv")
+GVE.RCP26.FF.DRY <- read.csv("GVE_2060_RCP26_DRY.csv")
+GVE.RCP85.FF.WS <- read.csv("GVE_2060_RCP85_1in10-warmsummer.csv")
+GVE.RCP85.FF.DRY <- read.csv("GVE_2060_RCP85_DRY.csv")
+
+
+setwd("/Code/Application/Data/DownloadedData/klimaszenarien-raumklima-INT")
+INT.RCP26.FF.WS <- read.csv("INT_2060_RCP26_1in10-warmsummer.csv")
+INT.RCP26.FF.DRY <- read.csv("INT_2060_RCP26_DRY.csv")
+INT.RCP85.FF.WS <- read.csv("INT_2060_RCP85_1in10-warmsummer.csv")
+INT.RCP85.FF.DRY <- read.csv("INT_2060_RCP85_DRY.csv")
+
+
+setwd("/Code/Application/Data/DownloadedData/klimaszenarien-raumklima-KLO")
+KLO.RCP26.FF.WS <- read.csv("KLO_2060_RCP26_1in10-warmsummer.csv")
+KLO.RCP26.FF.DRY <- read.csv("KLO_2060_RCP26_DRY.csv")
+KLO.RCP85.FF.WS <- read.csv("KLO_2060_RCP85_1in10-warmsummer.csv")
+KLO.RCP85.FF.DRY <- read.csv("KLO_2060_RCP85_DRY.csv")
+
+
+setwd("/Code/Application/Data/DownloadedData/klimaszenarien-raumklima-LUG")
+LUG.RCP26.FF.WS <- read.csv("LUG_2060_RCP26_1in10-warmsummer.csv")
+LUG.RCP26.FF.DRY <- read.csv("LUG_2060_RCP26_DRY.csv")
+LUG.RCP85.FF.WS <- read.csv("LUG_2060_RCP85_1in10-warmsummer.csv")
+LUG.RCP85.FF.DRY <- read.csv("LUG_2060_RCP85_DRY.csv")
+
+
+setwd("/Code/Application/Data/DownloadedData/klimaszenarien-raumklima-LUZ")
+LUZ.RCP26.FF.WS <- read.csv("LUZ_2060_RCP26_1in10-warmsummer.csv")
+LUZ.RCP26.FF.DRY <- read.csv("LUZ_2060_RCP26_DRY.csv")
+LUZ.RCP85.FF.WS <- read.csv("LUZ_2060_RCP85_1in10-warmsummer.csv")
+LUZ.RCP85.FF.DRY <- read.csv("LUZ_2060_RCP85_DRY.csv")
+
+
+setwd("/Code/Application/Data/DownloadedData/klimaszenarien-raumklima-MAG")
+MAG.RCP26.FF.WS <- read.csv("MAG_2060_RCP26_1in10-warmsummer.csv")
+MAG.RCP26.FF.DRY <- read.csv("MAG_2060_RCP26_DRY.csv")
+MAG.RCP85.FF.WS <- read.csv("MAG_2060_RCP85_1in10-warmsummer.csv")
+MAG.RCP85.FF.DRY <- read.csv("MAG_2060_RCP85_DRY.csv")
+
+
+setwd("/Code/Application/Data/DownloadedData/klimaszenarien-raumklima-MVE")
+# GVE.RCP85.NF.WS <- read.csv("GVE_2035_RCP85_1in10-warmsummer.csv")
+# GVE.RCP85.NF.DRY <- read.csv("GVE_2035_RCP85_DRY.csv")
+MVE.RCP26.FF.WS <- read.csv("MVE_2060_RCP26_1in10-warmsummer.csv")
+MVE.RCP26.FF.DRY <- read.csv("MVE_2060_RCP26_DRY.csv")
+MVE.RCP85.FF.WS <- read.csv("MVE_2060_RCP85_1in10-warmsummer.csv")
+MVE.RCP85.FF.DRY <- read.csv("MVE_2060_RCP85_DRY.csv")
+
+
+setwd("/Code/Application/Data/DownloadedData/klimaszenarien-raumklima-NABBER")
+NABBER.RCP26.FF.WS <- read.csv("NABBER_2060_RCP26_1in10-warmsummer.csv")
+NABBER.RCP26.FF.DRY <- read.csv("NABBER_2060_RCP26_DRY.csv")
+NABBER.RCP85.FF.WS <- read.csv("NABBER_2060_RCP85_1in10-warmsummer.csv")
+NABBER.RCP85.FF.DRY <- read.csv("NABBER_2060_RCP85_DRY.csv")
+
+setwd("/Code/Application/Data/DownloadedData/klimaszenarien-raumklima-NABLAU")
+NABLAU.RCP26.FF.WS <- read.csv("NABLAU_2060_RCP26_1in10-warmsummer.csv")
+NABLAU.RCP26.FF.DRY <- read.csv("NABLAU_2060_RCP26_DRY.csv")
+NABLAU.RCP85.FF.WS <- read.csv("NABLAU_2060_RCP85_1in10-warmsummer.csv")
+NABLAU.RCP85.FF.DRY <- read.csv("NABLAU_2060_RCP85_DRY.csv")
+
+
+setwd("/Code/Application/Data/DownloadedData/klimaszenarien-raumklima-OTL")
+OTL.RCP26.FF.WS <- read.csv("OTL_2060_RCP26_1in10-warmsummer.csv")
+OTL.RCP26.FF.DRY <- read.csv("OTL_2060_RCP26_DRY.csv")
+OTL.RCP85.FF.WS <- read.csv("OTL_2060_RCP85_1in10-warmsummer.csv")
+OTL.RCP85.FF.DRY <- read.csv("OTL_2060_RCP85_DRY.csv")
+
+
+setwd("/Code/Application/Data/DownloadedData/klimaszenarien-raumklima-PIO")
+PIO.RCP26.FF.WS <- read.csv("PIO_2060_RCP26_1in10-warmsummer.csv")
+PIO.RCP26.FF.DRY <- read.csv("PIO_2060_RCP26_DRY.csv")
+PIO.RCP85.FF.WS <- read.csv("PIO_2060_RCP85_1in10-warmsummer.csv")
+PIO.RCP85.FF.DRY <- read.csv("PIO_2060_RCP85_DRY.csv")
+
+
+
+setwd("/Code/Application/Data/DownloadedData/klimaszenarien-raumklima-PUY")
+PUY.RCP26.FF.WS <- read.csv("PUY_2060_RCP26_1in10-warmsummer.csv")
+PUY.RCP26.FF.DRY <- read.csv("PUY_2060_RCP26_DRY.csv")
+PUY.RCP85.FF.WS <- read.csv("PUY_2060_RCP85_1in10-warmsummer.csv")
+PUY.RCP85.FF.DRY <- read.csv("PUY_2060_RCP85_DRY.csv")
+
+
+
+setwd("/Code/Application/Data/DownloadedData/klimaszenarien-raumklima-REH")
+REH.RCP26.FF.WS <- read.csv("REH_2060_RCP26_1in10-warmsummer.csv")
+REH.RCP26.FF.DRY <- read.csv("REH_2060_RCP26_DRY.csv")
+REH.RCP85.FF.WS <- read.csv("REH_2060_RCP85_1in10-warmsummer.csv")
+REH.RCP85.FF.DRY <- read.csv("REH_2060_RCP85_DRY.csv")
+
+
+
+setwd("/Code/Application/Data/DownloadedData/klimaszenarien-raumklima-ROB")
+ROB.RCP26.FF.WS <- read.csv("ROB_2060_RCP26_1in10-warmsummer.csv")
+ROB.RCP26.FF.DRY <- read.csv("ROB_2060_RCP26_DRY.csv")
+ROB.RCP85.FF.WS <- read.csv("ROB_2060_RCP85_1in10-warmsummer.csv")
+ROB.RCP85.FF.DRY <- read.csv("ROB_2060_RCP85_DRY.csv")
+
+
+setwd("/Code/Application/Data/DownloadedData/klimaszenarien-raumklima-RUE")
+RUE.RCP26.FF.WS <- read.csv("RUE_2060_RCP26_1in10-warmsummer.csv")
+RUE.RCP26.FF.DRY <- read.csv("RUE_2060_RCP26_DRY.csv")
+RUE.RCP85.FF.WS <- read.csv("RUE_2060_RCP85_1in10-warmsummer.csv")
+RUE.RCP85.FF.DRY <- read.csv("RUE_2060_RCP85_DRY.csv")
+
+
+setwd("/Code/Application/Data/DownloadedData/klimaszenarien-raumklima-SAM")
+SAM.RCP26.FF.WS <- read.csv("SAM_2060_RCP26_1in10-warmsummer.csv")
+SAM.RCP26.FF.DRY <- read.csv("SAM_2060_RCP26_DRY.csv")
+SAM.RCP85.FF.WS <- read.csv("SAM_2060_RCP85_1in10-warmsummer.csv")
+SAM.RCP85.FF.DRY <- read.csv("SAM_2060_RCP85_DRY.csv")
+
+
+setwd("/Code/Application/Data/DownloadedData/klimaszenarien-raumklima-SBE")
+SBE.RCP26.FF.WS <- read.csv("SBE_2060_RCP26_1in10-warmsummer.csv")
+SBE.RCP26.FF.DRY <- read.csv("SBE_2060_RCP26_DRY.csv")
+SBE.RCP85.FF.WS <- read.csv("SBE_2060_RCP85_1in10-warmsummer.csv")
+SBE.RCP85.FF.DRY <- read.csv("SBE_2060_RCP85_DRY.csv")
+
+
+
+setwd("/Code/Application/Data/DownloadedData/klimaszenarien-raumklima-SCU")
+SCU.RCP26.FF.WS <- read.csv("SCU_2060_RCP26_1in10-warmsummer.csv")
+SCU.RCP26.FF.DRY <- read.csv("SCU_2060_RCP26_DRY.csv")
+SCU.RCP85.FF.WS <- read.csv("SCU_2060_RCP85_1in10-warmsummer.csv")
+SCU.RCP85.FF.DRY <- read.csv("SCU_2060_RCP85_DRY.csv")
+
+
+setwd("/Code/Application/Data/DownloadedData/klimaszenarien-raumklima-SHA")
+SHA.RCP26.FF.WS <- read.csv("SHA_2060_RCP26_1in10-warmsummer.csv")
+SHA.RCP26.FF.DRY <- read.csv("SHA_2060_RCP26_DRY.csv")
+SHA.RCP85.FF.WS <- read.csv("SHA_2060_RCP85_1in10-warmsummer.csv")
+SHA.RCP85.FF.DRY <- read.csv("SHA_2060_RCP85_DRY.csv")
+
+
+setwd("/Code/Application/Data/DownloadedData/klimaszenarien-raumklima-SIO")
+SIO.RCP26.FF.WS <- read.csv("SIO_2060_RCP26_1in10-warmsummer.csv")
+SIO.RCP26.FF.DRY <- read.csv("SIO_2060_RCP26_DRY.csv")
+SIO.RCP85.FF.WS <- read.csv("SIO_2060_RCP85_1in10-warmsummer.csv")
+SIO.RCP85.FF.DRY <- read.csv("SIO_2060_RCP85_DRY.csv")
+
+
+setwd("/Code/Application/Data/DownloadedData/klimaszenarien-raumklima-STG")
+STG.RCP26.FF.WS <- read.csv("STG_2060_RCP26_1in10-warmsummer.csv")
+STG.RCP26.FF.DRY <- read.csv("STG_2060_RCP26_DRY.csv")
+STG.RCP85.FF.WS <- read.csv("STG_2060_RCP85_1in10-warmsummer.csv")
+STG.RCP85.FF.DRY <- read.csv("STG_2060_RCP85_DRY.csv")
+
+
+setwd("/Code/Application/Data/DownloadedData/klimaszenarien-raumklima-ULR")
+ULR.RCP26.FF.WS <- read.csv("ULR_2060_RCP26_1in10-warmsummer.csv")
+ULR.RCP26.FF.DRY <- read.csv("ULR_2060_RCP26_DRY.csv")
+ULR.RCP85.FF.WS <- read.csv("ULR_2060_RCP85_1in10-warmsummer.csv")
+ULR.RCP85.FF.DRY <- read.csv("ULR_2060_RCP85_DRY.csv")
+
+
+setwd("/Code/Application/Data/DownloadedData/klimaszenarien-raumklima-VAD")
+VAD.RCP26.FF.WS <- read.csv("VAD_2060_RCP26_1in10-warmsummer.csv")
+VAD.RCP26.FF.DRY <- read.csv("VAD_2060_RCP26_DRY.csv")
+VAD.RCP85.FF.WS <- read.csv("VAD_2060_RCP85_1in10-warmsummer.csv")
+VAD.RCP85.FF.DRY <- read.csv("VAD_2060_RCP85_DRY.csv")
+
+
+setwd("/Code/Application/Data/DownloadedData/klimaszenarien-raumklima-WYN")
+WYN.RCP26.FF.WS <- read.csv("WYN_2060_RCP26_1in10-warmsummer.csv")
+WYN.RCP26.FF.DRY <- read.csv("WYN_2060_RCP26_DRY.csv")
+WYN.RCP85.FF.WS <- read.csv("WYN_2060_RCP85_1in10-warmsummer.csv")
+WYN.RCP85.FF.DRY <- read.csv("WYN_2060_RCP85_DRY.csv")
+
+
+setwd("/Code/Application/Data/DownloadedData/klimaszenarien-raumklima-ZER")
+ZER.RCP26.FF.WS <- read.csv("ZER_2060_RCP26_1in10-warmsummer.csv")
+ZER.RCP26.FF.DRY <- read.csv("ZER_2060_RCP26_DRY.csv")
+ZER.RCP85.FF.WS <- read.csv("ZER_2060_RCP85_1in10-warmsummer.csv")
+ZER.RCP85.FF.DRY <- read.csv("ZER_2060_RCP85_DRY.csv")
+
+#-------------------------------------------------------------------------------------
+#setwd("/Code/Application/Data/RawData")
+n.d <- dim(ZER.RCP26.FF.WS)[1]
+
+ID <- factor(rep(c("ABO", "AIG", "ALT", "BAS", "BER", "BKLI", "BUS", "CDF", "CHU", "DAV", "DIS", "ENG",
+                   "FRE", "GLA", "GSB", "GUT", "GVE", "INT", "KLO", "LUG", "LUZ", "MAG", "MVE", "NABBER",
+                   "NABLAU", "OTL", "PIO", "PUY", "REH", "ROB", "RUE", "SAM", "SBE", "SCU", "SHA", "SIO",
+                   "STG", "ULR", "VAD", "WYN", "ZER"), each=n.d))
+
+RCP26.FF.WS <- rbind(ABO.RCP26.FF.WS, AIG.RCP26.FF.WS, ALT.RCP26.FF.WS, BAS.RCP26.FF.WS, BER.RCP26.FF.WS,
+                     BKLI.RCP26.FF.WS, BUS.RCP26.FF.WS, CDF.RCP26.FF.WS, CHU.RCP26.FF.WS, DAV.RCP26.FF.WS,
+                     DIS.RCP26.FF.WS, ENG.RCP26.FF.WS, FRE.RCP26.FF.WS, GLA.RCP26.FF.WS, GSB.RCP26.FF.WS,
+                     GUT.RCP26.FF.WS, GVE.RCP26.FF.WS, INT.RCP26.FF.WS, KLO.RCP26.FF.WS, LUG.RCP26.FF.WS,
+                     LUZ.RCP26.FF.WS, MAG.RCP26.FF.WS, MVE.RCP26.FF.WS, NABBER.RCP26.FF.WS, NABLAU.RCP26.FF.WS,
+                     OTL.RCP26.FF.WS, PIO.RCP26.FF.WS, PUY.RCP26.FF.WS, REH.RCP26.FF.WS, ROB.RCP26.FF.WS,
+                     RUE.RCP26.FF.WS, SAM.RCP26.FF.WS, SBE.RCP26.FF.WS, SCU.RCP26.FF.WS, SHA.RCP26.FF.WS,
+                     SIO.RCP26.FF.WS, STG.RCP26.FF.WS, ULR.RCP26.FF.WS, VAD.RCP26.FF.WS, WYN.RCP26.FF.WS, ZER.RCP26.FF.WS)
+
+RCP26.FF.WS$ID <- ID
+
+
+RCP26.FF.DRY <- rbind(ABO.RCP26.FF.DRY, AIG.RCP26.FF.DRY, ALT.RCP26.FF.DRY, BAS.RCP26.FF.DRY, BER.RCP26.FF.DRY,
+                      BKLI.RCP26.FF.DRY, BUS.RCP26.FF.DRY, CDF.RCP26.FF.DRY, CHU.RCP26.FF.DRY, DAV.RCP26.FF.DRY,
+                      DIS.RCP26.FF.DRY, ENG.RCP26.FF.DRY, FRE.RCP26.FF.DRY, GLA.RCP26.FF.DRY, GSB.RCP26.FF.DRY,
+                      GUT.RCP26.FF.DRY, GVE.RCP26.FF.DRY, INT.RCP26.FF.DRY, KLO.RCP26.FF.DRY, LUG.RCP26.FF.DRY,
+                      LUZ.RCP26.FF.DRY, MAG.RCP26.FF.DRY, MVE.RCP26.FF.DRY, NABBER.RCP26.FF.DRY, NABLAU.RCP26.FF.DRY,
+                      OTL.RCP26.FF.DRY, PIO.RCP26.FF.DRY, PUY.RCP26.FF.DRY, REH.RCP26.FF.DRY, ROB.RCP26.FF.DRY,
+                      RUE.RCP26.FF.DRY, SAM.RCP26.FF.DRY, SBE.RCP26.FF.DRY, SCU.RCP26.FF.DRY, SHA.RCP26.FF.DRY,
+                      SIO.RCP26.FF.DRY, STG.RCP26.FF.DRY, ULR.RCP26.FF.DRY, VAD.RCP26.FF.DRY, WYN.RCP26.FF.DRY, ZER.RCP26.FF.DRY)
+
+RCP26.FF.DRY$ID <- ID
+
+
+
+RCP85.FF.WS <- rbind(ABO.RCP85.FF.WS, AIG.RCP85.FF.WS, ALT.RCP85.FF.WS, BAS.RCP85.FF.WS, BER.RCP85.FF.WS,
+                     BKLI.RCP85.FF.WS, BUS.RCP85.FF.WS, CDF.RCP85.FF.WS, CHU.RCP85.FF.WS, DAV.RCP85.FF.WS,
+                     DIS.RCP85.FF.WS, ENG.RCP85.FF.WS, FRE.RCP85.FF.WS, GLA.RCP85.FF.WS, GSB.RCP85.FF.WS,
+                     GUT.RCP85.FF.WS, GVE.RCP85.FF.WS, INT.RCP85.FF.WS, KLO.RCP85.FF.WS, LUG.RCP85.FF.WS,
+                     LUZ.RCP85.FF.WS, MAG.RCP85.FF.WS, MVE.RCP85.FF.WS, NABBER.RCP85.FF.WS, NABLAU.RCP85.FF.WS,
+                     OTL.RCP85.FF.WS, PIO.RCP85.FF.WS, PUY.RCP85.FF.WS, REH.RCP85.FF.WS, ROB.RCP85.FF.WS,
+                     RUE.RCP85.FF.WS, SAM.RCP85.FF.WS, SBE.RCP85.FF.WS, SCU.RCP85.FF.WS, SHA.RCP85.FF.WS,
+                     SIO.RCP85.FF.WS, STG.RCP85.FF.WS, ULR.RCP85.FF.WS, VAD.RCP85.FF.WS, WYN.RCP85.FF.WS, ZER.RCP85.FF.WS)
+
+RCP85.FF.WS$ID <- ID
+
+
+RCP85.FF.DRY <- rbind(ABO.RCP85.FF.DRY, AIG.RCP85.FF.DRY, ALT.RCP85.FF.DRY, BAS.RCP85.FF.DRY, BER.RCP85.FF.DRY,
+                      BKLI.RCP85.FF.DRY, BUS.RCP85.FF.DRY, CDF.RCP85.FF.DRY, CHU.RCP85.FF.DRY, DAV.RCP85.FF.DRY,
+                      DIS.RCP85.FF.DRY, ENG.RCP85.FF.DRY, FRE.RCP85.FF.DRY, GLA.RCP85.FF.DRY, GSB.RCP85.FF.DRY,
+                      GUT.RCP85.FF.DRY, GVE.RCP85.FF.DRY, INT.RCP85.FF.DRY, KLO.RCP85.FF.DRY, LUG.RCP85.FF.DRY,
+                      LUZ.RCP85.FF.DRY, MAG.RCP85.FF.DRY, MVE.RCP85.FF.DRY, NABBER.RCP85.FF.DRY, NABLAU.RCP85.FF.DRY,
+                      OTL.RCP85.FF.DRY, PIO.RCP85.FF.DRY, PUY.RCP85.FF.DRY, REH.RCP85.FF.DRY, ROB.RCP85.FF.DRY,
+                      RUE.RCP85.FF.DRY, SAM.RCP85.FF.DRY, SBE.RCP85.FF.DRY, SCU.RCP85.FF.DRY, SHA.RCP85.FF.DRY,
+                      SIO.RCP85.FF.DRY, STG.RCP85.FF.DRY, ULR.RCP85.FF.DRY, VAD.RCP85.FF.DRY, WYN.RCP85.FF.DRY, ZER.RCP85.FF.DRY)
+
+RCP85.FF.DRY$ID <- ID
+
+save(RCP26.FF.WS, file="RCP26_WS.Rdata")
+save(RCP26.FF.DRY, file="RCP26_DRY.Rdata")
+save(RCP85.FF.WS, file="RCP85_WS.Rdata")
+save(RCP85.FF.DRY, file="RCP85_DRY.Rdata")
+
+
